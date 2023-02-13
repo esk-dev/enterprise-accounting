f0b0d00b0b1745e5a13112b1303c2ab493fe1678
Egor <eesk98@gmail.com>
2023-02-13 11:44:35 +0000

    Update 5 files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  public isAuth!: Observable<boolean>;
-  constructor(private store: Store) {
-    this.isAuth = this.store.select(selectIsAuth);
-  }
-}
+export class AppComponent {}
